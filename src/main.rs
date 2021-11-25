d140357d2484a9c099dafb8f0665510a1ee9fd41
Colm Baston <colm@colmbaston.uk>
2021-11-25 18:25:47 +0000

    parsing and vectorising events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use json::JsonValue;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>>
@@ -6,12 +7,54 @@ fn main() -> Result<(), Box<dyn Error>>
     let session     = include_str!("../session.txt").trim_end();
     let leaderboard = include_str!("../leaderboard.txt").trim_end();
 
-    let client   = reqwest::blocking::Client::new();
-    let response = client.get(format!("https://adventofcode.com/{}/leaderboard/private/view/{}.json", year, leaderboard))
-                         .header("cookie", format!("session={}", session))
-                         .send()?;
+    let url  = format!("https://adventofcode.com/{}/leaderboard/private/view/{}.json", year, leaderboard);
+    let json = json::parse(&reqwest::blocking::Client::new().get(&url)
+                                                            .header("cookie", format!("session={}", session))
+                                                            .send()?
+                                                            .text()?)?;
 
-    println!("{}", response.text()?);
+    let events = vectorise_events(&json);
+
+    for e in events.iter()
+    {
+        println!("{:?}", e);
+    }
 
     Ok(())
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Event
+{
+    timestamp: u64,
+    name:      String,
+    day:       u8,
+    star:      u8
+}
+
+fn vectorise_events(json : &JsonValue) -> Vec<Event>
+{
+    let mut events = Vec::new();
+
+    for (_, member) in json["members"].entries()
+    {
+        let name = member["name"].to_string();
+
+        for (day, stars) in member["completion_day_level"].entries()
+        {
+            for (star, contents) in stars.entries()
+            {
+                events.push(Event
+                {
+                    timestamp: contents["get_star_ts"].to_string().parse().unwrap(),
+                    name:      name.clone(),
+                    day:       day.parse().unwrap(),
+                    star:      star.parse().unwrap(),
+                });
+            }
+        }
+    }
+
+    events.sort();
+    events
 }
